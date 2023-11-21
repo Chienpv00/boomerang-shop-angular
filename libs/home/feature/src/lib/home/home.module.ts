b01072d0dbb5e12d08bf238819690e6b8c679fea
chien.pham <chien.pham@spiraledge.com>
2023-11-21 23:40:30 +0700

    update router

--- libs/home/feature/src/lib/home/home.module.ts
+++ libs/home/feature/src/lib/home/home.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+    {
+      path: '',
+      component: HomeComponent
+    }
+  ];
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule.forChild(routes)],
     declarations: [HomeComponent],
     exports: [HomeComponent]
 })
